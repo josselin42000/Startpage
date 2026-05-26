5cb25303f594f4388803021c32482ad8a3ff1805
Josselin <josselin@l-h.fr>
2026-05-26 05:52:47 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import UserPage from './components/UserPage.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+var path = window.location.pathname
+var match = path.match(/^\/([a-zA-Z0-9-]+)\/?$/)
+
+if (match && match[1] !== 'index' && match[1] !== 'favicon') {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    React.createElement(UserPage, { slug: match[1] })
+  )
+} else {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    React.createElement(App, null)
+  )
+}
